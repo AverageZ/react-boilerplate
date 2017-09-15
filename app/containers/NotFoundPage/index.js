e08289e3f67249b5f5983340c2521a8af96698e6
AverageZ <alexzajac@live.com>
2017-09-15 18:45:36 -0400

    [internal] Regenerate NotFoundPage to match new internals

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -1,21 +1,38 @@
-/**
- * NotFoundPage
- *
- * This is the page we show when the user visits a url that doesn't have a route
- */
+/*
+*
+* NotFoundPage
+*
+*/
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Helmet from 'components/Helmet';
 
-import H1 from 'components/H1';
-import messages from './messages';
+export class NotFoundPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  }
 
-export default function NotFound() {
-  return (
-    <article>
-      <H1>
-        <FormattedMessage {...messages.header} />
-      </H1>
-    </article>
-  );
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="NotFoundPage"
+          meta={[
+            { name: 'description', content: 'Description of NotFoundPage' },
+          ]}
+        />
+      </div>
+    );
+  }
 }
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(null, mapDispatchToProps)(NotFoundPage);
